3f1fc1c9c5b16b68742868db2d98a675f348940a
Chris Patrick <ellemsee@gmail.com>
2017-08-06 15:39:39 -0400

    Update diagnosis_report_generator.R
    Add checks for when data is not available for generating reports

--- diagnosis_report_generator.R
+++ diagnosis_report_generator.R
@@ -51,26 +51,37 @@ if (!exists("summary_report")) {
     
     # Add a hard line break to each narrative so we have space between rows
     diagnoses$Narrative <- paste0(diagnoses$Narrative, "\\\n")
-  
+    
     # Change diagnosis report to include only a single submitter (if we are only testing the 
     # functionality rather than running all reports)
     if (test_report == "Y") diagnoses = diagnoses[diagnoses$SUBMITTERNAME == diagnoses$SUBMITTERNAME[1],]
-  
+    
     # Change diagnosis report to include only the submitters indicated if only_run is not ""
     if (!is.null(only_run)) diagnoses = filter(diagnoses, SUBMITTERNAME %in% only_run)
-  
+    
     # Get date range for report
     dates <- paste(gsub(" 0", " ", format(start_date, format="%B %d, %Y")), " - ", 
-                    gsub(" 0", " ", format(end_date, format="%B %d, %Y")), sep="")
-  
+                   gsub(" 0", " ", format(end_date, format="%B %d, %Y")), sep="")
+    
+    # Change column names
+    colnames(diagnoses)[2:4] <- c("Diagnosis", "Count", "Description")
+    
     # Generate report for each hospital
     render_file_diag <- paste(wd, slash, "diagnosis_markdown.Rmd", sep="")
-  
-    for (submitter in diagnoses$SUBMITTERNAME){
+    
+    for (submitter in diagnoses$SUBMITTERNAME) {
+      
+      # Subset dataframe
+      diag_sub = as.data.frame(diagnoses[diagnoses$SUBMITTERNAME == submitter, 2:4])
+      
+      # Change spacing for last row
+      diag_sub$Description[nrow(diag_sub)] <- gsub("\\\\\\n", "", diag_sub$Description[nrow(diag_sub)])
+      
+      # Create report
       rmarkdown::render(input = render_file_diag, 
                         output_format = "pdf_document",
                         output_file = paste(submitter, "_Diagnoses_", 
-                                          start_date, "_", end_date, ".pdf", sep=''),
+                                            start_date, "_", end_date, ".pdf", sep=''),
                         output_dir = output)
     } 
     
